d711f2ef68a1672a66ccfb43d4010ca55668cd23
pedro <pedro.ors2@gmail.com>
2024-04-01 22:48:05 -0300

    Time adjust for better performance and speed

--- 05 - Selenium Test/seleniumTest.py
+++ 05 - Selenium Test/seleniumTest.py
@@ -1,11 +1,23 @@
 import time
 from selenium import webdriver
+
+#This part of code it is for use on MS Edge WebDriver
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.edge.service import Service as EdgeService
+# edge_service = EdgeService(EdgeChromiumDriverManager().install())
+# driver = webdriver.Edge(service=edge_service)
 
-edge_service = EdgeService(EdgeChromiumDriverManager().install())
+#And this is for use on Google Chrome WebDriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Edge(service=edge_service)
+chrome_service = ChromeService(ChromeDriverManager().install())
+chrome_options = Options()
+# Add any additional options if needed
+# chrome_options.add_argument("--headless")  # Example of adding a headless argument
+
+driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
 driver.get('https://github.com/pedroors')
 
@@ -13,15 +25,24 @@ paginaPrincipal = driver.find_element("xpath", "/html/body/div[1]/div[4]/main/di
 
 paginaPrincipal.click()
 
-time.sleep(5.0)
+time.sleep(1.0)
 
-# menuRepositorios = driver.find_element("link text ", "WebScraping-Study")
+menuRepositorios = driver.find_element("xpath", "//*[@id='user-repositories-list']/ul/li[1]/div[1]/div[1]/h3/a")
 
-# menuRepositorios.click()
+menuRepositorios.click()
 
-# paginaPrincipalRepositorio = driver.find_element("xpath", "//*[@id='folder-row-5']/td[2]/div/div/h3/div/a")
+time.sleep(1.0)
 
-# paginaPrincipalRepositorio.click()
+paginaPrincipalRepositorio = driver.find_element("xpath", "//*[@id='folder-row-5']/td[2]/div/div/h3/div/a")
 
+paginaPrincipalRepositorio.click()
+
+time.sleep(1.0)
+
+paginaCodigo = driver.find_element("xpath", "//*[@id='folder-row-1']/td[2]/div/div/h3/div/a")
+
+paginaCodigo.click()
+
+time.sleep(2.0)
 
 driver.quit()
